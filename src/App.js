92ff5280d30ed0ebfe49fd5329abea8e845805c6
Pavel Nikolaev <pavel.nikolaev@tele2.ru>
2022-04-09 12:08:14 +0300

    Test hook

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test site
+          Test site 123
         </p>
       </header>
     </div>
